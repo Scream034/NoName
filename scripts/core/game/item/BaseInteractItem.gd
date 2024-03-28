09ac5d11e328095a656ec62e5b5b2f79917abdb3
paralax <kostgs034@gmail.com>
2024-03-28 22:44:41 +0500

    Нет старого проекта

--- scripts/core/game/item/BaseInteractItem.gd
+++
@@ -1,7 +0,0 @@
-extends BaseItem
-# NOTE: Черновик
-class_name BaseInteractItem
-
-signal process_use_started ## Когда предмет **начал процесс для использования**
-signal process_use_finished ## Когда предмет **закончил процесс для использования**
-signal used ## Когда предмет **использовался**
